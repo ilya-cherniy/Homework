ac70395cec99d269ea1d92b26f6ba41d7bccacd5
Ilya Cherniy <ilya.cherniy@gmail.com>
2019-12-14 16:19:01 +0200

    Add updates for Task 7

--- SearchInArray/SearchInArray.cs
+++ SearchInArray/SearchInArray.cs
@@ -20,10 +20,52 @@ namespace SearchInArray
             Random random = new Random();
             for (int i = 0; i < lengthOfArray; i++)
                 array[i] = random.Next(minBorder, maxBorder);
-            Console.WriteLine("Result array:{0} ", String.Join(" ", array));
+            Console.WriteLine("Generated array:{0} ", String.Join(" ", array));
 
-            //search min/max value
-            //for ....
+            int maxElement = array[0];
+            int maxPoint = 0;
+            int minElement = array[1];
+            int minPoint = 1;
+
+            for (int i = 0; i < lengthOfArray; i++)
+            {
+                if (array[i] > maxElement)
+                {
+                    maxElement = array[i];
+                    maxPoint = i;
+                }
+                if (array[i] < minElement)
+                {
+                    minElement = array[i];
+                    minPoint = i;
+                }
+                if (array[i] == maxPoint)
+                {
+                    array[i] = minElement;
+                }
+                if (array[i] == minPoint)
+                {
+                    array[i] = maxElement;
+                }
+            }
+
+
+            /*foreach (var item in array)
+            {
+                if (item > maxElement)
+                    maxElement = item;
+                if (item < minElement)
+                    minElement = item;
+            }
+            */
+            Console.WriteLine("Max element in array is: {0}", maxElement);
+            Console.WriteLine("Max element has position: {0}", maxPoint);
+
+            Console.WriteLine("Min element in array is: {0}", minElement);
+            Console.WriteLine("Min element has position: {0}", minPoint);
+
+            Console.WriteLine("Modified array:{0} ", String.Join(" ", array));
+            Console.ReadLine();
         }
     }
 }
