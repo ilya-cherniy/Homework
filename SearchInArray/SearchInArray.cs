399a93eae6d5b6f7eaa416c3ff67a327d3185521
Ilya Cherniy <ilya.cherniy@gmail.com>
2019-12-14 12:37:22 +0200

    Task 7 - Find Min\Max in array

---
+++ SearchInArray/SearchInArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchInArray
+{
+    class SearchInArray
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter length of array (N): ");
+            int lengthOfArray = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter min border of random range: ");
+            int minBorder = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter max border of random range: ");
+            int maxBorder = int.Parse(Console.ReadLine());
+            int[] array = new int[lengthOfArray];
+            Random random = new Random();
+            for (int i = 0; i < lengthOfArray; i++)
+                array[i] = random.Next(minBorder, maxBorder);
+            Console.WriteLine("Result array:{0} ", String.Join(" ", array));
+
+            //search min/max value
+            //for ....
+        }
+    }
+}
