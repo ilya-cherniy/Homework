fc549561047b3a4680b2c75b8a8bdc34c0ecf70d
Ilya Cherniy <ilya.cherniy@gmail.com>
2019-12-16 23:02:50 +0200

    Add task 14

---
+++ SearchCharacterInString14/SearchCharacterInString14.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SearchCharacterInString14
+{
+    class SearchCharacterInString14
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the text:");
+            string testString = Console.ReadLine();
+            Console.WriteLine("Please enter what do you find (character): ");
+            char testCharacter = char.Parse(Console.ReadLine());
+            int stringLength = testString.Length;
+            int searchResult = 0;
+            for (int i = 0; i < stringLength; i++)
+            {
+                if (testString[i] == testCharacter)
+                    searchResult ++;
+            }
+            if (searchResult == 0)
+                Console.WriteLine("Character {0}{1}", testCharacter," doesn't find");
+            else
+                Console.WriteLine("Character {0}{1}{2}{3}", testCharacter, "find ", searchResult, "times");
+            Console.ReadLine();
+        }
+    }
+}
