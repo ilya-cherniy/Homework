c058317725b6d25437459b527f38f8dac6f3cd93
Ilya Cherniy <ilya.cherniy@gmail.com>
2019-12-16 23:31:46 +0200

    Add task 16

---
+++ SearchSubStringInString16/SearchSubStringInString16.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SearchSubStringInString16
+{
+    class SearchSubStringInString16
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter test string:");
+            string testString = Console.ReadLine();
+            Console.WriteLine("Please enter sub-string: ");
+            string subString = Console.ReadLine();
+            bool result = testString.Contains(subString);
+            if (result)
+                Console.WriteLine("Test string contains sub-string");
+            else
+                Console.WriteLine("Sub-sting doesnt find");
+            Console.ReadLine();
+        }
+    }
+}
