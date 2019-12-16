2e2ef390f35b77bd9292f44734d5ae7c368fbced
Ilya Cherniy <ilya.cherniy@gmail.com>
2019-12-16 23:36:22 +0200

    Add task 17

---
+++ RemoveSubstringFromString17/RemoveSubstringFromString17.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RemoveSubstringFromString17
+{
+    class RemoveSubstringFromString17
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter test string:");
+            string testString = Console.ReadLine();
+            Console.WriteLine("Please enter sub-string: ");
+            string subString = Console.ReadLine();
+            testString = testString.Replace(subString, "");
+            Console.WriteLine("Modified string {0}", testString);
+            Console.ReadLine();
+        }
+    }
+}
