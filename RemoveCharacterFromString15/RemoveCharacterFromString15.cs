ffdf5c8b712fdf6fb730e6526277ce23d475aab0
Ilya Cherniy <ilya.cherniy@gmail.com>
2019-12-16 23:24:07 +0200

    Add task15

---
+++ RemoveCharacterFromString15/RemoveCharacterFromString15.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RemoveCharacterFromString15
+{
+    class RemoveCharacterFromString15
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the text:");
+            string testString = Console.ReadLine();
+            Console.WriteLine("Please enter what character do you want to delete: ");
+            string testCharacter = Console.ReadLine();
+            testString = testString.Replace(testCharacter, "");
+            Console.WriteLine("Modified string {0}", testString);
+            Console.ReadLine();
+        }
+    }
+}
