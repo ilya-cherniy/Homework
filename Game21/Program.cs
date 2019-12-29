d24106f5492fcc3b1516e8ea0a7881dfd4a1895b
Ilya Cherniy <ilya.cherniy@gmail.com>
2019-12-29 21:47:37 +0200

    Game 21 - Add few major updates

--- Game21/Program.cs
+++ Game21/Program.cs
@@ -33,41 +33,21 @@ namespace Game21
         }
         private static void Main(string[] args)
         {
+            //variables for program needs
+            int indexInDeck = 0;
+            int gamerScore = 0;
+            int computerScore = 0;
             Card[] deck = new Card[36];
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    deck[(i * 4) + j] = new Card();
-                    deck[(i * 4) + j].weight = (Weight)i;
-                    deck[(i * 4) + j].suit = (Suits)j;
-                }
-            }
-            //for (int i = 0; i < 36; i++)
-            //{
-            //    Console.WriteLine(deck[i].weight + " " + deck[i].suit);
-            //}
-                       
+            Card[] gamer = new Card[18];
+            Card[] computer = new Card[18];
             int indexCard;
-            Card[] tempCard = new Card[1];
-            Random random = new Random();
-            for (int j = 35; j > 0; j--)
-            {
-                indexCard = random.Next(j + 1);
-                tempCard[0] = deck[j];
-                deck[j] = deck[indexCard];
-                deck[indexCard] = tempCard[0];
-            }
-            Console.WriteLine("****************************************");
-            for (int i = 0; i < 36; i++)
-            {
-                Console.WriteLine(deck[i].weight + " " + deck[i].suit);
+            int gamerChoice = 0;
+            bool correctInput = false;
+            string firstPlayer;
+            string secondPlayer;
 
-            }
 
-            Card[] gamer = new Card[10];
-            Card[] computer = new Card[10];
-
+            //CardValue method will be used to calculate scores
             int CardValue(string card)
             {
                 int realValue = 0;
@@ -104,12 +84,95 @@ namespace Game21
                 return realValue;
             }
 
+            int DealCards(int numberOfCards, string playerName)
+            {
+                for (int i = indexInDeck; i < (numberOfCards+indexInDeck); i++)
+                {
+                    if (playerName == "gamer")
+                    {
+                        gamer[i] = deck[i];
+                        Console.WriteLine(gamer[i].weight + " " + gamer[i].suit);
+                        string cardValue = (gamer[i].weight).ToString();
+                        gamerScore = gamerScore + CardValue(cardValue);
+                    }
+                    else if (playerName == "computer")
+                    {
+                        computer[i] = deck[i];
+                        Console.WriteLine(computer[i].weight + " " + computer[i].suit);
+                        string cardValue = (computer[i].weight).ToString();
+                        computerScore = computerScore + CardValue(cardValue);
+                    }
+                }
+                indexInDeck += numberOfCards;
+                return indexInDeck;
+            }
+
+            int GamerDecision()
+            {
+                correctInput = false;
+                string playerInput = "";
+                Console.WriteLine("what do you want? Get [G] one more card or stop [S] receiving cards?");
+                while (!correctInput)
+                {
+                    playerInput = Console.ReadLine();
+                    if ((playerInput == "G") || (playerInput == "S"))
+                    {
+                        Console.WriteLine(playerInput);
+                        correctInput = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Entered value is not correct. Please try again: ");
+                    }
+                }
+
+                if (playerInput == "G")
+                {
+                    DealCards(1, firstPlayer);
+                }
+                else if (playerInput == "S")
+                {
+                    Console.WriteLine("Player decided to stay");
+                }
+
+                return gamerScore;
+            }
+
+            //generate an ordered deck of cards
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    deck[(i * 4) + j] = new Card();
+                    deck[(i * 4) + j].weight = (Weight)i;
+                    deck[(i * 4) + j].suit = (Suits)j;
+                }
+            }
+            //for (int i = 0; i < 36; i++)
+            //{
+            //    Console.WriteLine(deck[i].weight + " " + deck[i].suit);
+            //}
+
+            //shuffle a deck of cards
+            Card[] tempCard = new Card[1];
+            Random random = new Random();
+            for (int j = 35; j > 0; j--)
+            {
+                indexCard = random.Next(j + 1);
+                tempCard[0] = deck[j];
+                deck[j] = deck[indexCard];
+                deck[indexCard] = tempCard[0];
+            }
+            //print shuffled deck
+            //for (int i = 0; i < 36; i++)
+            //{
+            //    Console.WriteLine(deck[i].weight + " " + deck[i].suit);
+
+            //}
             Console.WriteLine("Please select [0] or [1] to decide who will start firts - Gamer or Computer");
-            string gamerInput = Console.ReadLine();
-            int gamerChoice = -1;
-            bool correctInput = false;
             while (!correctInput)
             {
+                string gamerInput = Console.ReadLine();
                 if (Int32.TryParse(gamerInput, out gamerChoice) && (gamerChoice == 0 || gamerChoice == 1))
                 {
                     Console.WriteLine(gamerChoice);
@@ -117,54 +180,28 @@ namespace Game21
                 }
                 else
                 {
-                    Console.WriteLine("Entered value is not correct");
+                    Console.WriteLine("Entered value is not correct. Please try again: ");
                 }
             }
-
             int randomCoin = random.Next(2);
             Console.WriteLine("Random coin: {0}", randomCoin);
             if (gamerChoice == randomCoin)
             {
                 Console.WriteLine("Gamer first");
+                firstPlayer = "gamer";
+                secondPlayer = "computer";
             }
             else
             {
                 Console.WriteLine("PC first");
+                firstPlayer = "computer";
+                secondPlayer = "gamer";
             }
-
-
-            //string gamerChoice = Console.ReadLine();
-            //int gCoin = -1;
-
 
             //give 2 cards to each gamer
             Console.WriteLine("****************************************");
-            int gamerScore = 0;
-            int computerScore = 0;
-            for (int i = 0; i < 4; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    gamer[i] = deck[i];
-                    //Console.WriteLine(gamer[i].weight + " " + gamer[i].suit);
-                    //string cardValue = (gamer[i].weight).ToString();
-                    //gamerScore = gamerScore + CardValue(cardValue);
-
-                }
-                else
-                {
-                    computer[i] = deck[i];
-                    //Console.WriteLine(computer[i].weight + " " + computer[i].suit);
-                    //string cardValue = (computer[i].weight).ToString();
-                    //computerScore = computerScore + CardValue(cardValue);
-                }
-                Console.WriteLine(gamer[i].weight + " " + gamer[i].suit);
-                string cardValuePlayer1 = (gamer[i].weight).ToString();
-                gamerScore = gamerScore + CardValue(cardValuePlayer1);
-                Console.WriteLine(computer[i].weight + " " + computer[i].suit);
-                string cardValuePlayer2 = (computer[i].weight).ToString();
-                computerScore = computerScore + CardValue(cardValuePlayer2);
-            }
+            DealCards(2, firstPlayer);
+            DealCards(2, secondPlayer);
 
             Console.WriteLine("Gamer score: {0}",gamerScore);
             Console.WriteLine("Computer score: {0}",computerScore);
@@ -174,29 +211,30 @@ namespace Game21
             {
                 Console.WriteLine("Gamer WINS");
             }
-            else if (computerScore == 22 || gamerScore == 21)
+            else if (computerScore == 22 || computerScore == 21)
             {
                 Console.WriteLine("Computer WINS");
 
             }
             else
             {
-                Console.WriteLine("what do you want? Get [G] one more card or stop [S] receiving cards?");
-                string gamerDecision = Console.ReadLine();
-                if (gamerDecision == "G")
+                
+                if (firstPlayer == "gamer")
                 {
-                    gamer[2] = deck[4];
-                    Console.WriteLine(gamer[2].weight + " " + gamer[2].suit);
-                    string cardValue = (gamer[2].weight).ToString();
-                    gamerScore = gamerScore + CardValue(cardValue);
+                    GamerDecision();
                     Console.WriteLine("Gamer score: {0}", gamerScore);
                 }
-
-                int computerDecision = random.Next(2);
-                if (computerScore <= 15)
+                else if (firstPlayer == "computer")
                 {
-                    //
+                    //TBD
+                    int computerDecision = random.Next(2);
+                    if (computerScore <= 15)
+                    {
+                        //
+                    }
                 }
+
+
             }  
             Console.ReadLine();
         }
