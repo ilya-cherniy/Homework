e66d9e7143125cdadda7badc4e94428f015083e7
Ilya Cherniy <ilya.cherniy@gmail.com>
2020-01-14 22:02:26 +0200

    Lection Collections

---
+++ CollectionsLect/MyCollection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionsLect
+{
+    class MyCollection : IEnumerable
+
+    {
+        
+        public MyCollection()
+        {
+
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new Myenumerator();
+        }
+    }
+}
---
+++ CollectionsLect/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace CollectionsLect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+}
